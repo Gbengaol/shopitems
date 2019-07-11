bdd148b7a7caf0ea9b4793d0ade0a40a1e435d27
GbengaO <gbenga.olufeyimi@3lineng.com>
2019-07-11 12:15:19 +0100

    Added signup with google details using firebase

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.util';
+import { auth, createUserProfileDocument } from './firebase/firebase.util'; //Importing from firebase.util.js
 
 class App extends React.Component{
   constructor(props){
@@ -15,18 +15,30 @@ class App extends React.Component{
     }
   }
 
-  unsubscribeFromAuth = null;
+  unsubscribeFromAuth = null; // Initial auth declaration 
 
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+  componentDidMount(){ //handling auth value (logged in or not) when a component mounts
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user)
+        await userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state)
+        });
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     })
   }
 
   componentWillUnmount (){
-    this.unsubscribeFromAuth();
+    this.unsubscribeFromAuth(); //Handling auth value (whether logged in or out) when a component unmounts
   }
 
   render(){
--- ecommerce/src/firebase/firebase.util.js
+++ ecommerce/src/firebase/firebase.util.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app'; //Import Firebase
 import 'firebase/firestore'; //For Firebase Database
 import 'firebase/auth'; //For Firebase Authentication
 
+// Configuration fiile is unique for every project created on Firebase
 
 const config = {
     apiKey: "AIzaSyDp48mmMnQ0YD4XX59xzYbPhlK_0WeMnyM",
@@ -13,13 +14,39 @@ const config = {
     appId: "1:733796509453:web:3658b9d696a98bc7"
   };
 
-firebase.initializeApp(config); // Firebase Configuration
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth){ return; }
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        
+        try {
+            await userRef.set ({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }
+        catch (error) {
+            console.log('error creating user', error.message)
+        }
+    } 
+    return userRef; 
+}
+
+firebase.initializeApp(config); // Firebase Configuration with the project config file
 
 export const auth = firebase.auth(); // To use firebase authenntication
 export const firestore = firebase.firestore(); // To use firebase database
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+const provider = new firebase.auth.GoogleAuthProvider(); //Using Google Firebase(Could also be Facebook, twitter, etc)
+provider.setCustomParameters({ prompt: 'select_account' }); //Prompt a select account window
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider); // To use Google for signin
 
